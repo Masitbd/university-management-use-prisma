a4d8a5eb671f72f1631327f89992ae4f1ebd9306
sarker24 <sarker24@gmail.com>
2023-08-21 12:55:05 +0200

    Create academic semester

---
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import { AcademicSemesterController } from './academicSemester.controller';
+const router = express.Router();
+
+router.post('/', AcademicSemesterController.insertIntoDB);
+export const AcademicSemesterRouter = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import { AcademicSemesterRouter } from '../modules/academicSemester/academicSemester.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
   // ... routes
   {
-    path: "",
-    routes: ""
-  }
+    path: '/academic-semesters',
+    route: AcademicSemesterRouter,
+  },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
