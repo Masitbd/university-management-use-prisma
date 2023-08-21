bf2d1c0ea782ca477be81b61d08f9dbe80b28816
sarker24 <sarker24@gmail.com>
2023-08-21 13:18:03 +0200

    Academic semester zod validation

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -1,6 +1,12 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
 export const AcademicSemesterRouter = router;
