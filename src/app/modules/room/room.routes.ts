c29c95bba27f994341ff21b46f3bd3d908c65ab1
sarker24 <sarker24@gmail.com>
2023-08-27 14:29:29 +0200

    Add semestration registration

--- src/app/modules/room/room.routes.ts
+++ src/app/modules/room/room.routes.ts
@@ -1,12 +1,28 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { BuildingValidations } from '../building/building.validations';
 import { RoomController } from './room.controller';
+import { RoomValidation } from './room.valiations';
 
 const router = express.Router();
 
 router.post(
   '/',
+  validateRequest(BuildingValidations.create),
   //  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   RoomController.insertIntoDB
+);
+router.patch(
+  '/:id',
+  validateRequest(RoomValidation.update),
+  //auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.updateOneInDB
+);
+
+router.delete(
+  '/:id',
+  //auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  RoomController.deleteByIdFromDB
 );
 
 export const roomRoutes = router;
